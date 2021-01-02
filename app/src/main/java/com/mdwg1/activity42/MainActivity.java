6d8ff2aada2d7ef5b6725dde91cd56d38fe12039
youssefbenjaber <youssefbenjaber@yahoo.com>
2021-01-02 12:11:54 +0100

    first page xml

---
+++ app/src/main/java/com/mdwg1/activity42/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mdwg1.activity42;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
